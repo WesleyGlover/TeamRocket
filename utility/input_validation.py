aba2c080f1c8dad4cdcffcc876bc067651616665
RedP1ggyBank <matthew.curtin2@gmail.com>
2022-04-07 21:01:28 -0500

    Update input_validation.py

---
+++ utility/input_validation.py
@@ -0,0 +1,34 @@
+#This script contains functions for validating user input
+#Matt Curtin 4/7/2022
+
+#Function for validating passwords
+#Input: password (string)
+#Output: True if valid, False if not
+#Password is valid if it is
+#   at least 8 characters long
+#   contains: >= 1 uppercase
+#   contains >=1 lowercase
+#   contains >= number
+#   contains >= special character
+def validate_password(password):
+
+    return True
+
+def validate_address(address):
+
+    return True
+
+#Function for validating usernames
+#Note: This does not test if username already exists in system. Just that it is correctly formatted
+#Input: email (string)
+#
+def validate_username(username):
+
+    return True
+
+#Function for validating email addresses
+#Note: This does not test if email already exists in system. Just that it is correctly formatted
+#Input: email (string)
+def validate_email(email):
+
+    return True
