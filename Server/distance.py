4b57499546380747a95ec40bc8d869b1911baabd
RedP1ggyBank <matthew.curtin2@gmail.com>
2022-04-19 20:53:58 -0500

    update distance.py to have functions

--- Server/distance.py
+++ Server/distance.py
@@ -6,43 +6,19 @@ from geopy.geocoders import Nominatim
 #https://towardsdatascience.com/driving-distance-between-two-or-more-places-in-python-89779d691def
 
 
+geolocator = Nominatim(user_agent="MITM")
 
-lat_1 = 33.218850
-lon_1 = -97.146090
-lat_2 = 33.210126
-lon_2 = -97.149010
-# call the OSMR API.
-r = requests.get(f"http://router.project-osrm.org/route/v1/car/{lon_1},{lat_1};{lon_2},{lat_2}?overview=false""")# then you load the response using the json libray
-# by default you get only one alternative so you access 0-th element of the `routes`
-routes = json.loads(r.content)
-route_1 = routes.get("routes")[0]
+#Input: Address as string
+#output: lat, lon
+def address_to_latlon(address):
 
-print(route_1["duration"])
+    location = geolocator.geocode(address).raw
 
-print(str(datetime.timedelta(seconds=route_1["duration"])))
+    return location['lat'], location['lon']
 
+def latlon_to_address(lat, lon):
+    location = geolocator.geocode(lat+","+lon)
+    return location
 
 
-#Addrerss to coordinates to distance
-app = Nominatim(user_agent="test")
-address1 = "1310 Scripture street, Denton, Texas 72601, United States"
-address2 = "332 East Hickory Street, Denton, Texas 76201, United States"
 
-#Transform location into coordinates
-location1 = app.geocode(address1).raw
-location2 = app.geocode(address2).raw
-
-print(location1['lat'], location1['lon'])
-print(location2['lat'], location2['lon'])
-
-#Use OSMR API to get route information
-r = requests.get(f"http://router.project-osrm.org/route/v1/car/{location1['lon']},{location1['lat']};{location2['lon']},{location2['lat']}?overview=false""")# then you load the response using the json libray
-
-# by default you get only one alternative so you access 0-th element of the `routes`
-routes = json.loads(r.content)
-route_1 = routes.get("routes")[0]
-
-#print duration in seconds of trip
-print(route_1["duration"])
-#print duration in hours, minutes, seconds, milliseconds format
-print(str(datetime.timedelta(seconds=route_1["duration"])))
