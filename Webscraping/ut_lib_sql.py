b4fc3c006adf9cecf812cfbcf6778ac30d63015e
Logan Wheeler <35555839+BraysonWheeler@users.noreply.github.com>
2022-05-04 19:08:09 -0500

    Add files via upload

---
+++ Webscraping/ut_lib_sql.py
@@ -0,0 +1,39 @@
+'''
+csv->df->list->sqltable
+'''
+import pandas as pd
+import mysql.connector
+
+
+df = pd.read_csv('./PLS_FY19_Outlet_pud19i.csv',encoding='latin-1')
+
+try:
+
+    connection = mysql.connector.connect(
+        user='doadmin',
+        password ='AVNS_WZEScW_Y5FNKr7m',
+        host='db-mysql-teamrocket-do-user-11106141-0.b.db.ondigitalocean.com',
+        port = 25060,
+        database='defaultdb'
+    )
+
+    cursor = connection.cursor()
+
+except BaseException as e:
+    print(str(e))
+lst = []
+for index,row in df.iterrows():
+    _streetAddr = str(row['ADDRESS'])
+    _mailState = str(row['STABR'])
+    _city = str(row['CITY'])
+
+    # LocationAddr = str(_city[1]) + ', ' + str(_state[1]) + ', ' + str(_streetAddr[1])
+    LocationAddr = _city + ', ' + _mailState + ', ' + _streetAddr
+    print(LocationAddr)
+
+    sqls = "INSERT INTO defaultdb.Locations(LocationName, LocationAddr) VALUES (%s, %s)"
+    val = ('Public Library', LocationAddr)
+    
+    cursor.execute(sqls,val)
+    connection.commit()
+
